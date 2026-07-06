d5c44b16ab947cc7bf147b80f3cf2a2da5966d8b
Naman <namangarg20021128@gmail.com>
2026-07-06 16:46:42 +0530

    Springboot Rest API's added

---
+++ Spring REST API SpringBoot/SpringBootRestAPI/src/main/java/com/naman/learning/SpringBootRestAPI/model/Product.java
@@ -0,0 +1,16 @@
+package com.naman.learning.SpringBootRestAPI.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    
+    private int prodId;
+    private String prodName;
+    private int price;
+
+
+}
